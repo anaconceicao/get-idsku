aa29f24670f82bd55e47db968690a6a82c757d6d
Ana Laura Silva <ana.conceicao@fcamara.com.br>
2020-07-21 15:47:41 -0300

    nova chamada

--- src/app/skuLeve.js
+++ src/app/skuLeve.js
@@ -10,7 +10,9 @@ console.log("*** API Funcionando! ***");
 
 sql.connect(connection, function(err) {
   if(err) console.log(err)
+})
 
+app.get('/', (req, res) => {
   let sqlRequest = new sql.Request();
 
   let sqlQuery = 'SELECT TOP(1) tt.Nome as tipotransporte, m.IdSkuOrigem FROM Inventario.SaldoEstoqueRestricao AS ser INNER JOIN Inventario.EstoqueRestricao as er ON(er.IdRestricao = ser.IdRestricao) LEFT JOIN Inventario.SaldoTipoEstoque AS ste ON(ste.IdSaldoTipoEstoque = ser.IdSaldoTipoEstoque) LEFT JOIN Inventario.SaldoSkuFilial AS sf ON(ste.IdSaldoSkuFilial = sf.IdSaldoSkuFilial) INNER JOIN Inventario.Modalidade m ON sf.IdSku = m.IdModalidadeSku left  join inventario.sku as s on m.IdModalidadeSku=s.id left join inventario.TipoTransporte as tt on tt.IdTipoTransporte=s.TipoTransporte WHERE m.TipoModalidade=0 and ser.QuantidadeDisponivel>0 and tt.IdTipoTransporte=1 ORDER BY NewID()'
@@ -24,10 +26,6 @@ sql.connect(connection, function(err) {
     console.log(idSkuOrigem)
     
     sql.close();
+    return res.json(idSkuOrigem)
   })
-  return idSkuOrigem;
-})
-
-app.get('/', (res, req) => {
-  req.json(idSkuOrigem)
 })
--- src/app/skuPesado.js
+++ src/app/skuPesado.js
@@ -11,10 +11,13 @@ console.log("API Funcionando!");
 sql.connect(connection, function(err) {
   if(err) console.log(err)
 
+})
+
+app.get('/', (req, res) => {
   let sqlRequest = new sql.Request();
 
   let sqlQuery = 'SELECT TOP(1) tt.Nome as tipotransporte, m.IdSkuOrigem FROM Inventario.SaldoEstoqueRestricao AS ser INNER JOIN Inventario.EstoqueRestricao as er ON(er.IdRestricao = ser.IdRestricao) LEFT JOIN Inventario.SaldoTipoEstoque AS ste ON(ste.IdSaldoTipoEstoque = ser.IdSaldoTipoEstoque) LEFT JOIN Inventario.SaldoSkuFilial AS sf ON(ste.IdSaldoSkuFilial = sf.IdSaldoSkuFilial) INNER JOIN Inventario.Modalidade m ON sf.IdSku = m.IdModalidadeSku left  join inventario.sku as s on m.IdModalidadeSku=s.id left join inventario.TipoTransporte as tt on tt.IdTipoTransporte=s.TipoTransporte where m.TipoModalidade=0 and ser.QuantidadeDisponivel>0 and tt.IdTipoTransporte=2 ORDER BY NewID()'
-
+  
   sqlRequest.query(sqlQuery, function(err, data) {
     if(err) console.log(err)
 
@@ -22,12 +25,7 @@ sql.connect(connection, function(err) {
 
     idSkuOrigem = JSON.parse(idSku.map(e => e.IdSkuOrigem))
     console.log(idSkuOrigem)
-    
-    sql.close();
-  })
-  return idSkuOrigem;
-})
+    return res.json(idSkuOrigem)
 
-app.get('/', (res, req) => {
-  req.json(idSkuOrigem)
+  })  
 })
